266b15e175087a477916a06709a8b290f95e0cba
Flavio <Flavio@localhost>
2017-08-29 21:43:07 -0300

    lendo um arquivo

--- app01/my-parser.js
+++ app01/my-parser.js
@@ -3,18 +3,35 @@ var fs = require('fs');
 
 
 // Leia o conteúdo do arquivo para a memória
-fs.readFile('exemple.log.txt', function ( err, loData ) {
+fs.readFile('exemple.log.txt', function (err, loData) {
 
     // Se um erro ocorrer, será lançada uma
     // exceção, e a aplicação irá ser encerrada
-    if ( err ){
+    if (err) {
         throw err;
 
-    } 
-    
+    }
+
 
     // logData é um Buffer, converta-o para string
     var text = loData.toString();
-    console.log(text)
+    var results = {};
+
+    // Quebrando o arquivo em linhas
+    var lines = text.split('\n');
+
+    lines.forEach(function (line) {
+        var parts = line.split(' '); // "timestamp letra numero"
+        var letter = parts[1];
+        var count = parseInt(parts[2]);
+
+        if (!results[letter]) {
+            results[letter] = 0;
+        }
+
+        results[letter] += parseInt(count);
+    });
+
+    console.log(results);
 
 });
