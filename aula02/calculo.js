fda7fb25d29e521eeb7afbed41bb27fa30cfd099
Flavio <Flavio@localhost>
2017-08-30 20:14:06 -0300

    modulos

--- aula02/calculo.js
+++ aula02/calculo.js
@@ -7,3 +7,4 @@ console.log( 'Um circulo de raio '+raio+' tem area de '+ circulo.area(raio));
 console.log( 'Um circulo de raio '+raio+' tem circunferencia de '+ circulo.circunferencia(raio));
 
 console.log(numberOne)
+console.log(numbertwo)
