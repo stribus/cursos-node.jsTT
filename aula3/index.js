d98c74e0b40068397176dae0ba25ba86d86f2be4
Flavio <Flavio@localhost>
2017-08-31 20:43:02 -0300

    teste express

--- aula3/index.js
+++ aula3/index.js
@@ -5,6 +5,15 @@ app.get('/',  (req, res) =>{
 
 })
 
+app.post('/',(req,res)=>{
+    res.send('Post recebido em /')
+})
+
+app.put('/users',(req,res)=>{res.send('PUT recebido em /users')})
+app.delete('/users',(req,res)=>{res.send('DELETE recebido em /users')})
+app.all('/secret',(req,res,next)=>{console.log('acessando secretamente');next()})
+app.get('/secret',(req,res)=>{res.send('acessando secretamente')})
+
 app.listen(3000,()=> {
     console.log('servidor rodando na porta 3000')
 })
