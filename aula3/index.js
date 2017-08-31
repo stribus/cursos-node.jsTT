8af1d1cf16ed37de6da306ed8f4be20d69a0a76a
Flavio <Flavio@localhost>
2017-08-31 20:11:24 -0300

    express --view=pug generated-app

--- aula3/index.js
+++ aula3/index.js
@@ -1,10 +1,10 @@
 var express = require('express');
 var app = express();
-app.get('/', function (req, res) {
+app.get('/',  (req, res) =>{
     res.send('hellow, express!')
 
 })
 
-app.listen(3000,function () {
+app.listen(3000,()=> {
     console.log('servidor rodando na porta 3000')
 })
